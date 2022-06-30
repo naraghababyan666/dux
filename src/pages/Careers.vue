9be03f8a80e1e6fd3588f16dbab1b1fb6f17d03f
HovhannesPiloyan3 <hovo.piloyan3@gmail.com>
2022-06-30 08:01:52 +0400

    career edit

--- src/pages/Careers.vue
+++ src/pages/Careers.vue
@@ -55,7 +55,7 @@
       </marquee>
     </div>
     <div class="devider">
-      <img src="../assets/images/careers/line.svg" alt="careers"/>
+      <img src="../assets/images/careers/benefites.svg" alt="careers"/>
     </div>
     <div class="benefits">
       <h2 class="benefits__title">We
@@ -81,29 +81,29 @@
         </div>
         <div class="row__container">
           <p class="row__text">
-              <span><img src="../assets/images/careers/dote.svg" alt="dote"></span>
+            <span><img src="../assets/images/careers/dote.svg" alt="dote"></span>
             Seafront offices.
           </p>
           <p class="row__text">
-              <span><img src="../assets/images/careers/dote.svg" alt="dote"></span>
+            <span><img src="../assets/images/careers/dote.svg" alt="dote"></span>
             Birthdays (we celebrate by giving employees birthday gifts + yummy treats).
           </p>
           <p class="row__text">
-              <span><img src="../assets/images/careers/dote.svg" alt="dote"></span>
+            <span><img src="../assets/images/careers/dote.svg" alt="dote"></span>
             Mobile phone allowance.
           </p>
         </div>
         <div class="row__container">
           <p class="row__text">
-              <span><img src="../assets/images/careers/dote.svg" alt="dote"></span>
+            <span><img src="../assets/images/careers/dote.svg" alt="dote"></span>
             Private health insurance.
           </p>
           <p class="row__text">
-              <span><img src="../assets/images/careers/dote.svg" alt="dote"></span>
+            <span><img src="../assets/images/careers/dote.svg" alt="dote"></span>
             Flexi-time.
           </p>
           <p class="row__text">
-              <span><img src="../assets/images/careers/dote.svg" alt="dote"></span>
+            <span><img src="../assets/images/careers/dote.svg" alt="dote"></span>
             Kitchen packed with free flowing treats/ fruit/ snacks.
           </p>
         </div>
@@ -115,8 +115,186 @@
     <div class="career">
       <h2 class="career__title">Start your
         <span class="color-blue"> star</span>
-          career at
+        career at
         <span class="color-blue">DUXGroup</span>
+      </h2>
+      <div class="career-main-block">
+        <h3 class="career-main-block-subtitle">Job openings</h3>
+        <h3 class="career-main-block-subtitle">Open Positions</h3>
+        <div class="career-main-block-items">
+          <p class="career-main-block-items-company-name">Affiliate Department</p>
+          <div class="career-main-block-items-text-container">
+            <p class="career-main-block-items-text-container-job-position">Junior Affiliate Manager</p>
+            <p class="career-main-block-items-text-container-job-hirer">Sliema, Malta</p>
+          </div>
+        </div>
+        <div class="career-main-block-items">
+          <p class="career-main-block-items-company-name">Affiliate Department</p>
+          <div class="career-main-block-items-text-container">
+            <p class="career-main-block-items-text-container-job-position">Junior Affiliate Manager</p>
+            <p class="career-main-block-items-text-container-job-hirer">Sliema, Malta</p>
+          </div>
+        </div>
+        <div class="career-main-block-items">
+          <p class="career-main-block-items-company-name">Affiliate Department</p>
+          <div class="career-main-block-items-text-container">
+            <p class="career-main-block-items-text-container-job-position">Junior Affiliate Manager</p>
+            <p class="career-main-block-items-text-container-job-hirer">Sliema, Malta</p>
+          </div>
+        </div>
+        <div class="career-main-block-items">
+          <p class="career-main-block-items-company-name">Affiliate Department</p>
+          <div class="career-main-block-items-text-container">
+            <p class="career-main-block-items-text-container-job-position">Junior Affiliate Manager</p>
+            <p class="career-main-block-items-text-container-job-hirer">Sliema, Malta</p>
+          </div>
+        </div>
+        <div class="career-main-block-items">
+          <p class="career-main-block-items-company-name">Affiliate Department</p>
+          <div class="career-main-block-items-text-container">
+            <p class="career-main-block-items-text-container-job-position">Junior Affiliate Manager</p>
+            <p class="career-main-block-items-text-container-job-hirer">Sliema, Malta</p>
+          </div>
+        </div>
+        <button class="career-main-block-button">see more <img src="../assets/images/careers/right.svg" alt="right">
+        </button>
+      </div>
+    </div>
+    <div class="devider">
+      <img src="../assets/images/careers/linesecond.svg" alt="line second"/>
+    </div>
+    <div class="stages">
+      <div class="stage-container">
+        <h2 class="stage-container-title">Stage 01</h2>
+        <p class="stage-container-sub-title">CV Screening</p>
+      </div>
+      <img class="line-svg" src="../assets/images/careers/Line.svg" alt="Line"/>
+      <div class="stage-container">
+        <h2 class="stage-container-title">Stage 02</h2>
+        <p class="stage-container-sub-title">1st Interview with HR</p>
+      </div>
+      <img class="line-svg" src="../assets/images/careers/Line.svg" alt="Line"/>
+      <div class="stage-container">
+        <h2 class="stage-container-title">Stage 03</h2>
+        <p class="stage-container-sub-title">Interview with Hiring Manager</p>
+        <p class="stage-container-sub-text">*Home Task may be included in this stage</p>
+      </div>
+      <img class="line-svg" src="../assets/images/careers/Line.svg" alt="Line"/>
+      <div class="stage-container">
+        <h2 class="stage-container-title">Stage 04</h2>
+        <p class="stage-container-sub-title">Final Interview with Management</p>
+        <p class="stage-container-sub-text">(Usually for high level position)</p>
+      </div>
+      <img class="line-svg" src="../assets/images/careers/Line.svg" alt="Line"/>
+      <div class="stage-container">
+        <h2 class="stage-container-title">Stage 05</h2>
+        <p class="stage-container-sub-title">Offer stage</p>
+      </div>
+    </div>
+    <div class="devider">
+      <img src="../assets/images/careers/about.svg" alt="about"/>
+    </div>
+    <div class="igaming">
+      <h2 class="igaming-title">
+        Innovative
+        <span class="color-blue">  iGaming</span>
+        Meets a Passion for Tech
+      </h2>
+      <div class="igaming-container">
+        <div class="igaming-container-left-side">
+          <div class="igaming-container-left-side-dote">
+            <img src="../assets/images/careers/dote16.svg" alt="dote16"/>
+          </div>
+          <div class="igaming-container-left-side-text">
+            <h3 class="igaming-container-left-side-title">We think several steps ahead</h3>
+          </div>
+        </div>
+        <div class="igaming-container-right-side">
+          <p class="igaming-container-right-side-text">
+            Hello world, DUXGroup calling! We’re a specialized team of
+            iGaming professionals that always manage to impress our customers and partners by providing the most
+            innovative and optimised iGaming solutions.Hello world, DUXGroup calling! We’re a specialized team of
+            iGaming professionals that always manage to impress our customers and partners by providing the most
+            innovative and optimised iGaming solutions.
+          </p>
+        </div>
+      </div>
+      <div class="igaming-container">
+        <div class="igaming-container-left-side">
+          <div class="igaming-container-left-side-dote">
+            <img src="../assets/images/careers/dote16.svg" alt="dote16"/>
+          </div>
+          <div class="igaming-container-left-side-text">
+            <h3 class="igaming-container-left-side-title">We take care about our people</h3>
+          </div>
+        </div>
+        <div class="igaming-container-right-side">
+          <p class="igaming-container-right-side-text">
+            Our people are not just employees. At DUXGroup, we’re a family! Everyone's an expert at what they do and we
+            work together to create the projects and products that both we and our customers are passionate about. We
+            live by the motto: "Improve, Perfect, Revolutionize!"
+          </p>
+        </div>
+      </div>
+      <div class="igaming-container">
+        <div class="igaming-container-left-side">
+          <div class="igaming-container-left-side-dote">
+            <img src="../assets/images/careers/dote16.svg" alt="dote16"/>
+          </div>
+          <div class="igaming-container-left-side-text">
+            <h3 class="igaming-container-left-side-title">We love what we do</h3>
+          </div>
+        </div>
+        <div class="igaming-container-right-side">
+          <p class="igaming-container-right-side-text">You want the most enthusiastic people in the iGaming industry?
+            You’ve found them! We pride ourselves on being self-sufficient because we only hire the best talent around.
+            Our DUXFamily is highly specialized, solves problems efficiently, and, most importantly, has a lot of fun
+            doing it!
+          </p>
+        </div>
+      </div>
+      <div class="igaming-container">
+        <div class="igaming-container-left-side">
+          <div class="igaming-container-left-side-dote">
+            <img src="../assets/images/careers/dote16.svg" alt="dote16"/>
+          </div>
+          <div class="igaming-container-left-side-text">
+            <h3 class="igaming-container-left-side-title">EVERYTHING YOU NEED TO WORK COMFORTABLY</h3>
+          </div>
+        </div>
+        <div class="igaming-container-right-side">
+          <p class="igaming-container-right-side-text">We could boast about our amazing seaview offices and other
+            amazing features you can expect when you join our company. But let’s be honest, the most important aspect of
+            work wellbeing has to do with the people you spend time with. Not only are our employees the most qualified
+            and professional at what they do; everybody is also very personable and you’ll fit in with us right away!
+          </p>
+        </div>
+      </div>
+      <div class="igaming-container">
+        <div class="igaming-container-left-side">
+          <div class="igaming-container-left-side-dote">
+            <img src="../assets/images/careers/dote16.svg" alt="dote16"/>
+          </div>
+          <div class="igaming-container-left-side-text">
+            <h3 class="igaming-container-left-side-title">ENGAGING EVENTS</h3>
+          </div>
+        </div>
+        <div class="igaming-container-right-side">
+          <p class="igaming-container-right-side-text">
+            Our teams are growing, with tons of people joining every month. To make sure everyone gets to know each
+            other, DUXGroup is organising many company events, giving everyone the chance to socialise and meet new
+            people, integrate into the teams, and build that family connection we value so much.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="title-container">
+      <h2 class="title">
+        We are always in <span class="color-blue">search of talented</span> and dedicated
+        <span class="color-blue">professionals.</span>
+        Let’s raise the
+        bar of possible through
+        <span class="color-blue">growth together.</span>
 
       </h2>
     </div>
@@ -142,6 +320,28 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
+.title-container{
+  background-color: #010C28;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 150px 115px;
+  & .title {
+
+    width: 1212px;
+    height: 244px;
+    font-family: 'Oxygen', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 58px;
+    line-height: 105%;
+    text-align: center;
+    letter-spacing: 0.02em;
+    text-transform: uppercase;
+    color: #F0F0F0;
+  }
+}
+
 
 .color-blue {
   color: #2F5BD8;
@@ -240,16 +440,19 @@ export default {
     color: #F0F0F0;
   }
 }
-.row{
+
+.row {
   width: 100%;
-  &__container{
+
+  &__container {
     display: flex;
     justify-content: space-around;
     width: 100%;
     margin-top: 72px;
-    & .row__text{
+
+    & .row__text {
       width: 374px;
-      font-family: 'Oxygen',sans-serif;
+      font-family: 'Oxygen', sans-serif;
       font-style: normal;
       font-weight: 400;
       font-size: 28px;
@@ -258,12 +461,14 @@ export default {
       text-transform: uppercase;
       color: #F0F0F0;
       display: flex;
-      & span{
+
+      & span {
         margin-right: 12px;
       }
     }
   }
 }
+
 .career {
   background-color: #010C28;
   display: flex;
@@ -274,7 +479,7 @@ export default {
 
   &__title {
     max-width: 820px;
-    font-family: 'Oxygen',sans-serif;
+    font-family: 'Oxygen', sans-serif;
     font-style: normal;
     font-weight: 400;
     font-size: 68px;
@@ -284,7 +489,222 @@ export default {
     text-transform: uppercase;
     color: #F0F0F0;
   }
+
+  &-main-block {
+    display: flex;
+    justify-content: flex-start;
+    width: 100%;
+    flex-direction: column;
+    padding: 26px;
+
+    &-subtitle {
+      font-family: 'Exo', sans-serif;
+      font-style: normal;
+      font-weight: 400;
+      font-size: 35px;
+      line-height: 47px;
+      color: #F0F0F0;
+      text-align: start;
+
+      &:nth-child(2) {
+        font-family: 'Exo', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 26px;
+        line-height: 125%;
+        color: #F0F0F0;
+      }
+    }
+
+    &-items {
+      border-bottom: 1px solid #828FA1;;
+      padding-bottom: 15px;
+
+      &-company-name {
+        margin-top: 15px;
+        font-family: 'Oxygen', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 18px;
+        letter-spacing: 0.04em;
+        color: #828FA1;
+
+      }
+
+      &-text-container {
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+
+        &-job-hirer {
+
+          font-family: 'Oxygen', sans-serif;
+          font-style: normal;
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 18px;
+          letter-spacing: 0.04em;
+          color: #828FA1;
+
+        }
+
+        &-job-position {
+          font-family: 'Exo', sans-serif;
+          font-style: normal;
+          font-weight: 400;
+          font-size: 20px;
+          line-height: 27px;
+          color: #F0F0F0;
+          margin-top: 5px;
+        }
+      }
+    }
+
+    &-button {
+      background: none;
+      font-family: 'Oxygen', sans-serif;
+      display: flex;
+      font-style: normal;
+      font-weight: 700;
+      font-size: 14px;
+      border: none;
+      line-height: 105%;
+      letter-spacing: 0.04em;
+      text-transform: uppercase;
+      color: #F0F0F0;
+      margin-top: 42px;
+    }
+  }
 }
+
+.stages {
+  background-color: #010C28;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 96px 25px 0 25px;
+  justify-content: space-between;
+
+  & .line-svg {
+    margin: 0 10px;
+  }
+
+  & .stage-container {
+
+    width: 228px;
+    height: 228px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border: 1px solid #828FA1;
+    border-radius: 10px;
+
+    &-title {
+      font-family: 'Oxygen', sans-serif;
+      font-style: normal;
+      font-weight: 400;
+      font-size: 28px;
+      line-height: 35px;
+      text-align: center;
+      letter-spacing: 0.03em;
+      text-transform: uppercase;
+      color: #F0F0F0;
+    }
+
+    &-sub-title {
+      font-family: 'Exo', sans-serif;
+      font-style: normal;
+      font-weight: 400;
+      font-size: 20px;
+      line-height: 27px;
+      text-align: center;
+      color: #F0F0F0;
+      margin-top: 8px;
+    }
+
+    &-sub-text {
+      margin-top: 8px;
+      font-family: 'Oxygen', sans-serif;
+      font-style: normal;
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 105%;
+      text-align: center;
+      letter-spacing: 0.04em;
+      text-transform: uppercase;
+      color: #F0F0F0;
+    }
+  }
+}
+
+.igaming {
+  background-color: #010C28;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &-title {
+    max-width: 820px;
+    text-align: center;
+    font-family: 'Oxygen', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 58px;
+    line-height: 105%;
+
+    letter-spacing: 0.02em;
+    text-transform: uppercase;
+    color: #F0F0F0;
+  }
+
+  &-container {
+    margin-top: 70px;
+    padding: 0 25px;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    border-top: 1px solid #828FA1;
+
+    &-left-side {
+      display: flex;
+
+      &-dote {
+        margin-right: 102px;
+        padding-top: 10px;
+      }
+
+      &-title {
+        max-width: 472px;
+        font-family: 'Oxygen', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 38px;
+        line-height: 48px;
+        letter-spacing: 0.03em;
+        text-transform: uppercase;
+        color: #F0F0F0;
+      }
+    }
+
+    &-right-side {
+      &-text {
+        max-width: 562px;
+        font-family: 'Exo', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 27px;
+        color: #F0F0F0;
+
+      }
+    }
+  }
+
+}
+
 .footer {
   background-color: #010C28;
 }
